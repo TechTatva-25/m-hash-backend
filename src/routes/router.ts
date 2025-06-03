a6115704ef83cc4866df44bdd386400ae72324e5
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-06-03 11:58:23 +0530

    Routes added to router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,15 @@
 import express from "express";
 
 import authRoute from "./auth.route";
+import teamRoute from "./team.route";
+import inviteRoute from "./invite.route";
+import problemRoute from "./problem.route";
+import collegeRoute from "./college.route";
 
 export function initRoutes(app: express.Application): void {
-	app.use("/api/auth", authRoute);
+  app.use("/api/auth", authRoute);
+  app.use("/api/team", teamRoute);
+  app.use("/api/invite", inviteRoute);
+  app.use("/api/problem", problemRoute);
+  app.use("/api/college", collegeRoute);
 }
