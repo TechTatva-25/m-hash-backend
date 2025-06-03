4df0aaea46929b0763f39a3c4edbbe91e6f3126e
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-06-03 09:39:33 +0530

    Created Problem model

---
+++ src/models/Problem/index.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IProblem extends Document {
+  title: string;
+  description: string;
+  thumbnail: string;
+  features: string[];
+  type: string;
+  sdg_id: number;
+  sdg_title: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ProblemSchema: Schema<IProblem> = new Schema<IProblem>(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    features: { type: [String], required: true },
+    type: { type: String, required: true },
+    sdg_id: { type: Number, required: true },
+    sdg_title: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IProblem>("Problem", ProblemSchema);
