ee9d5d861f584b96f07e354e8defebf75d33cc00
Tanishq Kochar <tanishqkochar73@gmail.com>
2025-06-05 06:38:52 +0530

    Judge Routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,9 @@
 import express from "express";
 
 import authRoute from "./auth.route";
+import judgeRoute from "./auth.route";
 
 export function initRoutes(app: express.Application): void {
 	app.use("/api/auth", authRoute);
+	app.use("/api/judge",judgeRoute);
 }
