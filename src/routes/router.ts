0e2b84bf46ecf4f8a4126390bebd3d5eb0df65ca
Tanishq Kochar <tanishqkochar73@gmail.com>
2025-06-09 15:57:33 +0530

    Minor Edit in router.ts

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import authRoute from "./auth.route";
-import judgeRoute from "./auth.route";
+import judgeRoute from "./judge.route";
+import adminRoute from "./admin.route"
 
 export function initRoutes(app: express.Application): void {
 	app.use("/api/auth", authRoute);
 	app.use("/api/judge",judgeRoute);
+	app.use("/api/admin",adminRoute);
 }
