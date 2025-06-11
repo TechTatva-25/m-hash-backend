e1777bbe9158f97af3394b1a08731e30dccfee6e
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-06-11 10:23:30 +0530

    Ticket routes added

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -7,6 +7,7 @@ import teamRoute from "./team.route";
 import inviteRoute from "./invite.route";
 import problemRoute from "./problem.route";
 import collegeRoute from "./college.route";
+import ticketRoute from "./ticket.route";
 
 export function initRoutes(app: express.Application): void {
   app.use("/api/auth", authRoute);
@@ -16,4 +17,5 @@ export function initRoutes(app: express.Application): void {
   app.use("/api/invite", inviteRoute);
   app.use("/api/problem", problemRoute);
   app.use("/api/college", collegeRoute);
+  app.use("/api/ticket", ticketRoute);
 }
